58326e9c8bb257ad16245524261b77a386dfd697
AllanCapistrano <allan.capistrano3@gmail.com>
2023-08-12 10:36:43 -0300

    chore: rename file

---
+++ src/main/java/br/uefs/larsid/iot/soft/Main.java
@@ -0,0 +1,12 @@
+package br.uefs.larsid.iot.soft;
+
+/**
+ * @author Allan Capistrano
+ * @version 1.0.0
+ */
+public class Main {
+
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}
