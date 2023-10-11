c14e197fac3528d642fac2a6f4e1a525f6b48a9a
AllanCapistrano <allan.capistrano3@gmail.com>
2023-10-11 10:47:29 -0300

    feat: create LedgerReader class

---
+++ src/main/java/br/uefs/larsid/iot/soft/models/LedgerReader.java
@@ -0,0 +1,200 @@
+package br.uefs.larsid.iot.soft.models;
+
+import br.uefs.larsid.iot.soft.models.tangle.Payload;
+import br.uefs.larsid.iot.soft.models.transactions.Evaluation;
+import br.uefs.larsid.iot.soft.models.transactions.Transaction;
+// import br.uefs.larsid.iot.soft.services.ILedgerSubscriber;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+// import java.util.HashMap;
+import java.util.List;
+// import java.util.Map;
+// import java.util.Set;
+import java.util.logging.Logger;
+
+import org.iota.jota.error.ArgumentException;
+
+/**
+ * @author Allan Capistrano
+ */
+public class LedgerReader implements Runnable {
+
+  /*-------------------------Constantes---------------------------------------*/
+  private static final long SLEEP = 5000;
+  private static final String ENDPOINT = "message";
+  private static final String ENDPOINT_MESSAGE_ID = "message/messageId";
+  /*--------------------------------------------------------------------------*/
+
+  private Thread ledgerReader;
+  private final String index;
+  private boolean debugModeValue;
+  private String urlApi;
+  // private Thread DLTInboundMonitor;
+  // private final Map<String, Set<ILedgerSubscriber>> topics;
+
+  private static final Logger logger = Logger.getLogger(
+    LedgerReader.class.getName()
+  );
+
+  public LedgerReader(
+    String protocol,
+    String url,
+    int port,
+    String index,
+    boolean debugModeValue
+  ) {
+    this.urlApi = String.format("%s://%s:%s", protocol, url, port);
+    // this.topics = new HashMap<String, Set<ILedgerSubscriber>>();
+    this.debugModeValue = debugModeValue;
+    this.index = index;
+
+    if (this.ledgerReader == null) {
+      this.ledgerReader = new Thread(this);
+      this.ledgerReader.setName("TANGLE_MONITOR/LEDGER_READER");
+      this.ledgerReader.start();
+    }
+  }
+
+  /**
+   * Obtêm todas as transações a partir de um índice específico.
+   *
+   * @param index String - Índice das transações.
+   * @return List<Transaction>
+   */
+  public List<Transaction> getTransactionsByIndex(String index) {
+    String response = null;
+    List<Transaction> transactions = new ArrayList<Transaction>();
+
+    try {
+      URL url = new URL(
+        String.format("%s/%s/%s", this.urlApi, ENDPOINT, index)
+      );
+      HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+      if (conn.getResponseCode() != HttpURLConnection.HTTP_OK) {
+        throw new RuntimeException(
+          "HTTP error code : " + conn.getResponseCode()
+        );
+      }
+
+      BufferedReader br = new BufferedReader(
+        new InputStreamReader((conn.getInputStream()))
+      );
+
+      String temp = null;
+
+      while ((temp = br.readLine()) != null) {
+        response = temp;
+      }
+
+      conn.disconnect();
+
+      transactions =
+        Transaction.jsonArrayInStringToTransaction(response, debugModeValue);
+
+      return transactions;
+    } catch (MalformedURLException mue) {
+      if (debugModeValue) {
+        logger.severe(mue.getMessage());
+      }
+    } catch (IOException ioe) {
+      if (debugModeValue) {
+        logger.severe(ioe.getMessage());
+      }
+    }
+
+    return transactions;
+  }
+
+  /**
+   * Obtém uma transação a partir de um ID.
+   *
+   * @param transactionId String - ID da transação.
+   * @return Transaction
+   */
+  public Transaction getTransactionById(String transactionId) {
+    String response = null;
+
+    try {
+      URL url = new URL(
+        String.format(
+          "%s/%s/%s/",
+          this.urlApi,
+          ENDPOINT_MESSAGE_ID,
+          transactionId
+        )
+      );
+      HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+      if (conn.getResponseCode() != HttpURLConnection.HTTP_OK) {
+        throw new RuntimeException(
+          "HTTP error code : " + conn.getResponseCode()
+        );
+      }
+
+      BufferedReader br = new BufferedReader(
+        new InputStreamReader((conn.getInputStream()))
+      );
+
+      String temp = null;
+
+      while ((temp = br.readLine()) != null) {
+        response = temp;
+      }
+
+      conn.disconnect();
+
+      return Transaction.getTransactionObjectByType(
+        Payload.stringToPayload(response).getData(),
+        debugModeValue
+      );
+    } catch (MalformedURLException mue) {
+      if (debugModeValue) {
+        logger.severe(mue.getMessage());
+      }
+    } catch (IOException ioe) {
+      if (debugModeValue) {
+        logger.severe(ioe.getMessage());
+      }
+    }
+
+    return null;
+  }
+
+  /**
+   * Thread para obter a(s) transação/transações.
+   */
+  @Override
+  public void run() {
+    while (!this.ledgerReader.isInterrupted()) {
+      try {
+        // Gson gson = new Gson();
+        // Evaluation transactionn = gson.fromJson(
+        //   "{\"value\":0}",
+        //   Evaluation.class
+        // );
+        // logger.info(transactionn.toString());
+
+        long start = System.currentTimeMillis();
+
+        for (Transaction transaction : this.getTransactionsByIndex(this.index)) {
+          logger.info(((Evaluation) transaction).toString());
+        }
+
+        long end = System.currentTimeMillis();
+        logger.info("API Response time (ms): " + (end - start));
+
+        Thread.sleep(SLEEP);
+      } catch (ArgumentException ae) {
+        logger.warning(ae.getStackTrace().toString());
+      } catch (InterruptedException ie) {
+        logger.warning(ie.getStackTrace().toString());
+      }
+    }
+  }
+}
