70d8747caa6244b0d79123a0c1c35608a8368994
AllanCapistrano <allan.capistrano3@gmail.com>
2024-05-21 10:39:31 -0300

    chore: updating the uptade method

--- src/main/java/br/uefs/larsid/iot/soft/services/ILedgerSubscriber.java
+++ src/main/java/br/uefs/larsid/iot/soft/services/ILedgerSubscriber.java
@@ -5,5 +5,5 @@ package br.uefs.larsid.iot.soft.services;
  * @author Allan Capistrano
  */
 public interface ILedgerSubscriber {
-  public void update(Object object);
+  public void update(Object object, Object object2);
 }
