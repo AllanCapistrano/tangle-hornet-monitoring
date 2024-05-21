b0370f6298142c84c28b2873240487758d8ce806
AllanCapistrano <allan.capistrano3@gmail.com>
2024-05-21 10:35:41 -0300

    feat: creating a class to publish transaction in the blockchain

---
+++ src/main/java/br/uefs/larsid/iot/soft/models/ZMQPublisher.java
@@ -0,0 +1,180 @@
+package br.uefs.larsid.iot.soft.models;
+
+import br.uefs.larsid.iot.soft.models.enums.TransactionType;
+import br.uefs.larsid.iot.soft.models.transactions.Evaluation;
+import br.uefs.larsid.iot.soft.models.transactions.IndexTransaction;
+import br.uefs.larsid.iot.soft.models.transactions.Transaction;
+import com.google.gson.Gson;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Allan Capistrano
+ * @version 1.0.0
+ */
+public class ZMQPublisher implements Runnable {
+
+  private String urlApi;
+  private Thread DLTOutboundMonitor;
+  private boolean debugModeValue;
+  private final BlockingQueue<IndexTransaction> DLTOutboundBuffer;
+  private static final String ENDPOINT = "message";
+
+  private static final Logger logger = Logger.getLogger(
+    ZMQPublisher.class.getName()
+  );
+
+  public ZMQPublisher(
+    String protocol,
+    String url,
+    int port,
+    int bufferSize,
+    boolean debugModeValue
+  ) {
+    this.urlApi = String.format("%s://%s:%s", protocol, url, port);
+    this.DLTOutboundBuffer =
+      new ArrayBlockingQueue<IndexTransaction>(bufferSize);
+
+    this.debugModeValue = debugModeValue;
+
+    if (this.DLTOutboundMonitor == null) {
+      this.DLTOutboundMonitor = new Thread(this);
+      this.DLTOutboundMonitor.setName("TANGLE_MONITOR/ZMQ_PUBLISHER");
+      this.DLTOutboundMonitor.start();
+    }
+  }
+
+  @Override
+  public void run() {
+    Gson gson = new Gson();
+
+    while (!this.DLTOutboundMonitor.isInterrupted()) {
+      try {
+        Transaction transaction = new Evaluation(
+          "fakeSourceZMQ",
+          "fakeTargetZMQ",
+          TransactionType.REP_ZMQ_MONITOR,
+          0,
+          System.currentTimeMillis(),
+          System.currentTimeMillis()
+        );
+
+        this.put(
+            new IndexTransaction(
+              TransactionType.REP_ZMQ_MONITOR.toString(),
+              transaction
+            )
+          );
+
+        IndexTransaction indexTransaction = this.DLTOutboundBuffer.take();
+
+        indexTransaction
+          .getTransaction()
+          .setPublishedAt(System.currentTimeMillis());
+
+        String transactionJson = gson.toJson(indexTransaction.getTransaction());
+
+        this.createMessage(indexTransaction.getIndex(), transactionJson);
+      } catch (InterruptedException ex) {
+        this.DLTOutboundMonitor.interrupt();
+      }
+    }
+  }
+
+  /**
+   * Put a transaction to be published on Tangle Hornet
+   *
+   * @param indexTransaction IndexTransaction - Transação que será publicada.
+   */
+  public void put(IndexTransaction indexTransaction)
+    throws InterruptedException {
+    this.DLTOutboundBuffer.put(indexTransaction);
+  }
+
+  /**
+   * Create a new message in Tangle Hornet.
+   *
+   * @param index String - Index of the message.
+   * @param data String - Data of the message.
+   */
+  public void createMessage(String index, String data) {
+    try {
+      URL url = new URL(String.format("%s/%s", this.urlApi, ENDPOINT));
+
+      /* Open HTTP connection. */
+      HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+      connection.setRequestMethod("POST");
+      connection.setRequestProperty("Content-Type", "application/json");
+      connection.setDoOutput(true); // Permitir a escrita no corpo da requisição
+
+      String requestBody = String.format(
+        "{\"index\": \"%s\",\"data\": %s}",
+        index,
+        data
+      );
+
+      /* Writes request body to OutputStream */
+      try (
+        DataOutputStream outputStream = new DataOutputStream(
+          connection.getOutputStream()
+        )
+      ) {
+        outputStream.writeBytes(requestBody);
+        outputStream.flush();
+      }
+
+      /* Receives the response */
+      int responseCode = connection.getResponseCode();
+
+      /* Reads API response */
+      if (responseCode == HttpURLConnection.HTTP_OK) {
+        BufferedReader in = new BufferedReader(
+          new InputStreamReader(connection.getInputStream())
+        );
+        String inputLine;
+        StringBuilder response = new StringBuilder();
+
+        while ((inputLine = in.readLine()) != null) {
+          response.append(inputLine);
+        }
+        in.close();
+
+        if (debugModeValue) {
+          logger.info("API response: " + response.toString());
+        }
+      } else {
+        if (debugModeValue) {
+          logger.info("Error in HTTP request: " + responseCode);
+        }
+      }
+
+      /* Close the connection. */
+      connection.disconnect();
+    } catch (IOException ioe) {
+      if (debugModeValue) {
+        logger.severe(ioe.getMessage());
+      }
+    }
+  }
+
+  public String getUrl() {
+    return urlApi;
+  }
+
+  public void setUrlApi(String urlApi) {
+    this.urlApi = urlApi;
+  }
+
+  public void setDebugModeValue(boolean debugModeValue) {
+    this.debugModeValue = debugModeValue;
+  }
+}
